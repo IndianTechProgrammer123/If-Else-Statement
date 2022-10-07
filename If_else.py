b9d752f65f8ef3294bfdbd17d3d858d6efaad8ba
Indian Tech Programmer <112259649+IndianTechProgrammer123@users.noreply.github.com>
2022-10-07 17:47:38 +0530

    Update If_else.py

--- If_else.py
+++ If_else.py
@@ -2,12 +2,17 @@
 
 # Why Use If Else Statement?
 # Execute Both The True Part And The False Part Of a Given Condition
-
-# Please Like Share And Comment on The Video And Subscribe to The Channel 
-# Watch Full Video
-
-Ex = 1
+Ex = 3
 if Ex>2:
     print('big')
 else:
     print('small')
+
+num = int(input("enter a num : "))  
+if num == 1:
+    print("num = 1") 
+else:
+    print("Other")
+    
+# Please Like Share And Comment on The Video And Subscribe to The Channel 
+# Watch Full Video
