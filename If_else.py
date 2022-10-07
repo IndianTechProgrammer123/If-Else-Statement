0cb0a6e60ebe9fd54b7547390794418598850cab
Indian Tech Programmer <112259649+IndianTechProgrammer123@users.noreply.github.com>
2022-10-07 13:17:44 +0530

    Add files via upload

---
+++ If_else.py
@@ -0,0 +1,13 @@
+# In This Video, we learn about If and Else Statement
+
+# Why Use If Else Statement?
+# Execute Both The True Part And The False Part Of a Given Condition
+
+# Please Like Share And Comment on The Video And Subscribe to The Channel 
+# Watch Full Video
+
+Ex = 1
+if Ex>2:
+    print('big')
+else:
+    print('small')
